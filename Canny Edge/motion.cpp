1829a1055cb257ee6babe6387f82fac179e46e68
srli <sophia.li@students.olin.edu>
2014-07-09 11:55:09 -0400

    trying to implement graph

--- Canny Edge/motion.cpp
+++ Canny Edge/motion.cpp
@@ -65,18 +65,43 @@ int main(int argc, char** argv){
 			GaussianBlur(sobel, gaussian, Size(5,5), 2, 2);
 			Canny(gaussian, dest, 40, 350, 3);
 
-			Mat element = getStructuringElement(MORPH_CROSS, Size(5, 5), Point(2, 2));
+			std::vector<std::vector<Point> > contours;
+			std::vector<Vec4i> hierarchy;
+
+			findContours(dest, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0));
+
+/*			for( int i = 0; i < contours.size(); i++){
+				drawContours( dest_eroded, contours, i, Scalar(255, 0, 0), 2, 8, hierarchy, 0, Point());
+			}*/
+
+/*			Mat element = getStructuringElement(MORPH_CROSS, Size(5, 5), Point(2, 2));
 			dilate(dest, dest_eroded, element);
 	
 			//addWeighted(dest_eroded, 0.5, previous3, 0.5, 0.0, overlay);
 
 			bitwise_or(dest_eroded, gray, overlay);
-
-			previous1 = dest_eroded;
+*/
+			/*previous1 = dest_eroded;
 			previous2 = previous1;
-			previous3 = previous2;
+			previous3 = previous2;*/
+/*
+			std::vector<Vec2f> lines;
 
-			imshow("Canny Edge", previous3);
+			HoughLines(dest_eroded, lines, 1, CV_PI/180, 50);
+
+			std::cout << "number of lines: " << lines.size() << std::endl;
+
+			for(size_t i = 0; i < lines.size(); i++){
+
+				line(dest_eroded, Point(lines[i][0], lines[i][1]), Point(lines[i][2], lines[i][3]), Scalar(0,255,0), 1, 8);
+
+			}
+
+*/
+
+			copyMakeBorder( dest_eroded, overlay, 20, 20, 20, 20, BORDER_CONSTANT, Scalar(255, 255, 255));
+
+		//	imshow("Canny Edge", dest_eroded);
 			imshow("Overlay", overlay);
 
 /*	
@@ -85,7 +110,7 @@ int main(int argc, char** argv){
 			imshow("Canny Edge", dest_eroded);
 			imshow("Gaussian", gaussian);
 */
-			c = cvWaitKey(1000);
+			c = cvWaitKey(10);
 			if (c == 27){
 				break;
 
