9cd5d39244dd7c52e14d64180fb1979a63ad0531
srli <sophia.li@students.olin.edu>
2014-07-10 15:36:14 -0400

    broken code, fixing

--- Canny Edge/motion.cpp
+++ Canny Edge/motion.cpp
@@ -16,6 +16,7 @@ int main(int argc, char** argv){
 	Mat src, gray, sobel, gaussian, canny_output, eroded, overlay_color, overlay;
 	Mat contours, eroded_raw;
 	Mat previous;
+	double minVal; double maxVal; Point minLoc; Point maxLoc;
 
 	IplImage* color_img;
 	CvCapture* cv_cap = cvCreateFileCapture("underwater1.webm"); //previous video
@@ -28,24 +29,21 @@ int main(int argc, char** argv){
 
 	std::vector<int> distance_vector;
 
-	int z = 0;
+	bool start = true; 
 
 	for(;;){
 		color_img = cvQueryFrame(cv_cap);
 
-		z += 1;
-
 		if (color_img != 0){
 			src = color_img;
-
-
 
 			//Changing color image to gray
 			cvtColor(src, gray, CV_BGR2GRAY);
 			
-			if (z == 1){
+			if (start){
 				printf("Initialize\n");
 				previous = gray;
+				start = false;
 			}
 
 			//Sobel filter for horizontal lines, then canny to detect edges	
@@ -71,19 +69,30 @@ int main(int argc, char** argv){
 
 			std::vector<std::vector<Point> > contours;
 			std::vector<Vec4i> hierarchy;
+			std::vector<int> maxy_values;
 
 			// Find contours
 			findContours( canny_output, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0) );
   			// Draw contours
 			//std::cout << contours.size() << std::endl;
 			if (contours.size() > 20){
-				imshow("eroded", previous);
+				//imshow("eroded", previous);
 				goto draw_previous;
 			}
+
+
 
 			for( int i = 0; i < contours.size(); i++ ){
 				if(contours[i].size() > 5){
 					drawContours( drawing, contours, i, Scalar(255, 255, 255), 2, 8, hierarchy, 0, Point() );
+					std::vector<int> y_values;
+					int max_y;
+					for(int j = 0; j < contours[i].size(); j++){
+						y_values.push_back(contours[i][j].y);
+					}
+					max_y = *std::max_element(y_values.begin(), y_values.end());
+					maxy_values.push_back(max_y);
+
 				}
 /*				else{
 					drawContours( drawing, contours, i, Scalar(255, 255, 255), 2, 8, hierarchy, 0, Point() );
@@ -94,8 +103,11 @@ int main(int argc, char** argv){
 			
 			cvtColor(eroded_raw, eroded, CV_BGR2GRAY);
  			
+ 			eroded = previous;
 
-
+/*					minMaxLoc( eroded_raw, &minVal, &maxVal, &minLoc, &maxLoc);
+					std::cout << minVal << std::endl;
+*/
 			for (int i = 0; i < eroded.cols; i++){
 				for (int j = 0; j < eroded.rows; j++){
 					int intensity = (int)eroded.at<uchar>(j, i);
@@ -109,9 +121,8 @@ int main(int argc, char** argv){
 				}
 			}
 
-			imshow("eroded", eroded);
-			previous = eroded;
-
+			imshow("original", src);
+			
 			cvtColor(eroded, overlay_color, CV_GRAY2BGR);
 			
  			copyMakeBorder(overlay_color, overlay, border, border, border, border, BORDER_CONSTANT, Scalar(255, 255, 255));
@@ -119,9 +130,21 @@ int main(int argc, char** argv){
 			line(overlay, Point(border,border), Point(border, image_size.height + border + 10), Scalar(0,255,0), 3, 8, 0);
 			line(overlay, Point(border - 10,image_size.height + border), Point(image_size.width + border, image_size.height + border), Scalar(0,255,0), 3, 8, 0);
 			
-			
-			draw_previous:
-			
+			for (int z = 0; z < maxy_values.size(); z++){
+				line(overlay, Point(border, maxy_values[z] + border), Point(image_size.width + border, maxy_values[z] + border), Scalar(0,255,255), 2, 8, 0);
+				char text[255];
+				sprintf(text, "Distance %d", maxy_values[z]);
+				putText(overlay, text, Point(border, maxy_values[z] + 10), 
+    								FONT_HERSHEY_COMPLEX_SMALL, 0.8, Scalar(200,200,250), 1, CV_AA);
+			}
+
+			int total_max = *std::max_element(maxy_values.begin(), maxy_values.end());
+			char text1[255];
+			sprintf(text1, "Closests %d", total_max);
+			putText(overlay, text1, Point(image_size.width -100, border), 
+    			FONT_HERSHEY_COMPLEX_SMALL, 0.8, Scalar(200,200,250), 1, CV_AA);
+
+			draw_previous:						
 			// Show in a window
 			imshow("drawing", overlay);
 
@@ -133,8 +156,8 @@ int main(int argc, char** argv){
 
 			}
 		}
-	}
 	
+
 	cvReleaseCapture( &cv_cap);
 	cvDestroyWindow("Video");
 	return 0;
