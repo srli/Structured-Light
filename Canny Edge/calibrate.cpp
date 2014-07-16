301ee98442e7e27c50c0a3202344718537f417c6
srli <sophia.li@students.olin.edu>
2014-07-16 16:31:00 -0400

    adding calibration script

--- Canny Edge/calibrate.cpp
+++ Canny Edge/calibrate.cpp
@@ -31,14 +31,35 @@ int main(int argc, char** argv){
 	Canny(gray, canny_output, 40, 350, 3);
 	Mat element = getStructuringElement(MORPH_CROSS, Size(5, 5), Point(2, 2));
 	dilate(canny_output, canny_eroded, element);
+
+	std::vector<Vec2f> lines2;
+	HoughLines(canny_eroded, lines2, 1, CV_PI/180, 50);
+
+	std::cout << "number of lines: " << lines2.size() << std::endl;
+
+	for(size_t i = 0; i < lines2.size(); i++)
+	{
+		float rho = lines2[i][0];
+		float theta = lines2[i][1];
+
+		
+		double a = cos(theta), b = sin(theta);
+		double x0 = a*rho, y0 = b*rho;
+		Point pt1(cvRound(x0 + 1000*(-b)),
+				cvRound(y0 + 1000*(a)));
+		Point pt2(cvRound(x0 - 1000*(-b)),
+				cvRound(y0 - 1000*(a)));
+
+		line(src, pt1, pt2, Scalar(0,0,255), 2, 8);
+	}
 	
-	std::vector<Vec4i> lines;
+/*	std::vector<Vec4i> lines;
 	HoughLinesP(canny_eroded, lines, 1, CV_PI/180, 80, 150, 2);
 
 	for(size_t i=0; i < lines.size(); i++){
 		//Drawing each line that we've found
 		line(src, Point(lines[i][0], lines[i][1]), Point(lines[i][2], lines[i][3]), Scalar(0,255,0), 2, 8);
-	}
+	}*/
 
 	imshow("src", src);
 	//imshow("dst", dst);
